85ee140ece06cff219b5c2515486a61778641225
erjiaqing <gs199704@gmail.com>
2015-02-15 11:03:23 +0800

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # Danmuku
 Danmuku / "弹幕"
+
+# TODO
+
+[ ] Colorful danmuku
+
+# How to
+
+First at all you have to get it: `git clone https://github.com/erjiaqing/Danmuku.git`
+
+Then, install necessary packages: `cd Danmuku && npm install`
+
+For this is just a beta version, you need to edit some lines:
+
+`show.html` : 
+
+`L7` : `<canvas id="danmuku" width="500" height="300"></canvas>` edit the value of `width`, `height` to the size you want
+
+`L13` : `var socket = io.connect('http://localhost:8080');` edit the url to `http://IP:8080`
+
+`index.html` :
+
+`L59` : `var socket = io.connect('http://192.168.1.5:8080');` edit like `L13 @ show.html`
+
+Finally, `node app.js` to startup the danmuku server
+
+# License
+
+MIT
---
+++ app.js
@@ -0,0 +1,36 @@
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(8080);
+
+var uidStr = new String("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ=+");
+
+function getUid(uti) {
+  var ret = new String();
+  while (uti > 0) {
+    ret = uidStr.charAt(uti % 64) + ret;
+    uti = ( ( uti - uti % 64 ) / 64);
+  }
+  while (ret.length < 10) {
+    ret = '0' + ret;
+  }
+  return ret;
+}
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/show', function (req, res) {
+  res.sendFile(__dirname + '/show.html');
+});
+
+io.on('connection', function (socket) {
+  var curTime = new Date();
+  socket.emit('message-hello', { server: 'EJQDanmuku' , uid: getUid(curTime)});
+  socket.on('dmk', function (msg) {
+    console.log(msg);
+    io.emit('message', { uid: msg.uid , danmuku: msg.cont });
+  });
+});
